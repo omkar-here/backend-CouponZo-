ba52b2190648ca25e87d7c3853de44d4f7ebb871
Skyhero-alt <skyhero.code@gmail.com>
2023-08-16 20:34:59 +0530

    updated urls

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const couponRoutes = require("./routes/Coupon");
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://couponzo.netlify.app/",
+    ],
     credentials: true,
   })
 );
