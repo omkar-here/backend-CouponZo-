63006e21572fce5921bb7c7562b76a30360cf04f
Omkar Anabathula <aomkarg2003@gmail.com>
2023-09-04 00:44:53 +0530

    mess

--- middleware/authMiddleWare.js
+++ middleware/authMiddleWare.js
@@ -3,7 +3,9 @@ const User= require("../models/User");
 const verify = (req, res) => {
   try {
     // console.log(req.cookies)
-    const token = req.cookies.jwt;
+    // const token = req.cookies.jwt;
+    const token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZTVmZDEwMzI1ZDJiZjgzZWQyNWIyYSIsImlhdCI6MTY5Mzc2ODM0NywiZXhwIjoyNTU3NzY4MzQ3fQ.YO5nwKOFd_0rMQbdmDQ2skcee5f--xak_eAVUqtLIgU";
+
 
     //check json web token exists & is verified
     if (token) {
